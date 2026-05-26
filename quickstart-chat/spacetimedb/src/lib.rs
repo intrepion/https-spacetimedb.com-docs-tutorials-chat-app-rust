1cfbe13898b9f82af25673322c62bfd7c11be04c
Oliver Forral <oliver.forral@myreason.ai>
2026-05-25 17:37:29 -0700

    Send messages

--- quickstart-chat/spacetimedb/src/lib.rs
+++ quickstart-chat/spacetimedb/src/lib.rs
@@ -33,3 +33,23 @@ fn validate_name(name: String) -> Result<String, String> {
         Ok(name)
     }
 }
+
+#[reducer]
+pub fn send_message(ctx: &ReducerContext, text: String) -> Result<(), String> {
+    let text = validate_message(text)?;
+    log::info!("{}", text);
+    ctx.db.message().insert(Message {
+        sender: ctx.sender(),
+        text,
+        sent: ctx.timestamp,
+    });
+    Ok(())
+}
+
+fn validate_message(text: String) -> Result<String, String> {
+    if text.is_empty() {
+        Err("Messages must not be empty".to_string())
+    } else {
+        Ok(text)
+    }
+}
