1ea9a73decc92ae14d0efe1de0ef20916bdc1a92
Oliver Forral <oliver.forral@myreason.ai>
2026-05-25 18:52:37 -0700

    Define the main function

--- quickstart-chat/src/main.rs
+++ quickstart-chat/src/main.rs
@@ -1,3 +1,20 @@
 mod module_bindings;
 use module_bindings::*;
 use spacetimedb_sdk::{credentials, DbContext, Error, Event, Identity, Status, Table, TableWithPrimaryKey};
+
+fn main() {
+    // Connect to the database
+    let ctx = connect_to_db();
+
+    // Register callbacks to run in response to database events.
+    register_callbacks(&ctx);
+
+    // Subscribe to queries in order to construct a local partial replica of the database.
+    subscribe_to_tables(&ctx);
+
+    // Spawn a thread, where the connection will process messages and invoke callbacks.
+    ctx.run_threaded();
+
+    // Handle CLI input
+    user_input_loop(&ctx);
+}
