580abf0197b3454d11247920617470b01f732a76
Oliver Forral <oliver.forral@myreason.ai>
2026-05-25 17:36:43 -0700

    Define tables

--- quickstart-chat/spacetimedb/src/lib.rs
+++ quickstart-chat/spacetimedb/src/lib.rs
@@ -1,1 +1,16 @@
 use spacetimedb::{table, reducer, Table, ReducerContext, Identity, Timestamp};
+
+#[table(accessor = user, public)]
+pub struct User {
+    #[primary_key]
+    identity: Identity,
+    name: Option<String>,
+    online: bool,
+}
+
+#[table(accessor = message, public)]
+pub struct Message {
+    sender: Identity,
+    sent: Timestamp,
+    text: String,
+}
