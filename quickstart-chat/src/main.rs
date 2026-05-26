f9a5310f8b946d8c076985c4b797a3246bd7f845
Oliver Forral <oliver.forral@myreason.ai>
2026-05-25 18:53:01 -0700

    Connect to the database

--- quickstart-chat/src/main.rs
+++ quickstart-chat/src/main.rs
@@ -18,3 +18,31 @@ fn main() {
     // Handle CLI input
     user_input_loop(&ctx);
 }
+
+/// The URI of the SpacetimeDB instance hosting our chat database and module.
+const HOST: &str = "http://localhost:3000";
+
+/// The database name we chose when we published our module.
+const DB_NAME: &str = "quickstart-chat";
+
+/// Load credentials from a file and connect to the database.
+fn connect_to_db() -> DbConnection {
+    DbConnection::builder()
+        // Register our `on_connect` callback, which will save our auth token.
+        .on_connect(on_connected)
+        // Register our `on_connect_error` callback, which will print a message, then exit the process.
+        .on_connect_error(on_connect_error)
+        // Our `on_disconnect` callback, which will print a message, then exit the process.
+        .on_disconnect(on_disconnected)
+        // If the user has previously connected, we'll have saved a token in the `on_connect` callback.
+        // In that case, we'll load it and pass it to `with_token`,
+        // so we can re-authenticate as the same `Identity`.
+        .with_token(creds_store().load().expect("Error loading credentials"))
+        // Set the database name we chose when we called `spacetime publish`.
+        .with_database_name(DB_NAME)
+        // Set the URI of the SpacetimeDB host that's running our database.
+        .with_uri(HOST)
+        // Finalize configuration and connect!
+        .build()
+        .expect("Failed to connect")
+}
