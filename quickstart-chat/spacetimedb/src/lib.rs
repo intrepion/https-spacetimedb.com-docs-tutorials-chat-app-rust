525f60331beb2fa65f80fd97f8316442462285fc
Oliver Forral <oliver.forral@myreason.ai>
2026-05-25 17:37:08 -0700

    Set users' names

--- quickstart-chat/spacetimedb/src/lib.rs
+++ quickstart-chat/spacetimedb/src/lib.rs
@@ -14,3 +14,22 @@ pub struct Message {
     sent: Timestamp,
     text: String,
 }
+
+#[reducer]
+pub fn set_name(ctx: &ReducerContext, name: String) -> Result<(), String> {
+    let name = validate_name(name)?;
+    if let Some(user) = ctx.db.user().identity().find(ctx.sender()) {
+        ctx.db.user().identity().update(User { name: Some(name), ..user });
+        Ok(())
+    } else {
+        Err("Cannot set name for unknown user".to_string())
+    }
+}
+
+fn validate_name(name: String) -> Result<String, String> {
+    if name.is_empty() {
+        Err("Names must not be empty".to_string())
+    } else {
+        Ok(name)
+    }
+}
